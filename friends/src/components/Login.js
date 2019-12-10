7d90ce8ee257b72de3667d2ae83db36a1ce39016
john <john@spinningplates.org>
2019-12-09 17:45:27 -0800

    Login page return token to localStorage

---
+++ friends/src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+    console.log(credentials)
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', credentials)
+      .then(res => {
+        console.log('Login.js login: res', res);
+        localStorage.setItem('token', res.data.payload);
+      })
+      .catch(err => console.log('Login.js error: ', err));
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
